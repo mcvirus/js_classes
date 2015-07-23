96365596980c518fabbc357dde3dea151ab6d3de
Зубко Виталий <vortaxg@gmail.com>
2015-07-23 14:10:01 +0300

    exercise 6.3 complete

--- js/tasks.js
+++ js/tasks.js
@@ -238,4 +238,58 @@ Object.defineProperty(Vector.prototype, "length", {
   //  console.log(new Vector(1, 2).minus(new Vector(2, 3)));
 // → Vector{x: -1, y: -1}
     //console.log(new Vector(3, 4).length);
-    // → 5
+// → 5
+
+//6-3
+
+function logFive(sequence) {
+    for (var i = 0; i < 5; i++) {
+        if (!sequence.next()) {
+            break;
+        }
+        console.log(sequence.current());
+    }
+}
+
+function ArraySeq(array) {
+    this.pos = -1;
+    this.array = array;
+}
+
+ArraySeq.prototype.next = function() {
+    if (this.pos >= this.array.length - 1) {
+        return false;
+    }
+    this.pos++;
+    return true;
+}
+ArraySeq.prototype.current = function() {
+    return this.array[this.pos];
+}
+
+function RangeSeq(from, to) {
+    this.pos = from - 1;
+    this.to = to;
+
+}
+
+RangeSeq.prototype.next = function() {
+    if (this.pos > this.to) {
+        return false;
+    }
+    this.pos++;
+    return true;
+}
+RangeSeq.prototype.current = function () {
+    return this.pos;
+}
+
+//logFive(new ArraySeq([1, 2]));
+// → 1
+// → 2
+//logFive(new RangeSeq(100, 1000));
+// → 100
+// → 101
+// → 102
+// → 103
+// → 104
